f20e5b39e10bf5c907c7383d03620d55eab23587
Jason Goncalves <jasongoncalves@Jasons-MacBook-Pro.local>
2018-09-17 23:18:22 -0400

    added api/read and api/read_single

--- config/Database.php
+++ config/Database.php
@@ -1,7 +1,7 @@
 <?php
     class Database {
         // db params
-        private $host = 'localhost';
+        private $host = 'localhost:8889';
         private $db_name = 'example';
         private $username = 'root';
         private $password = 'root';
@@ -12,7 +12,7 @@
             $this->conn = null;
 
             try {
-                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password);
+                $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
                 $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
             } catch(PDOException $e) {
                 echo 'Connection Error: ' . $e->getMessage();
